70580d2d0b749c3e3502c4deb0e6cb337748c66a
Soumya Sharma <soumya13106@iiitd.ac.in>
2019-04-01 01:05:41 +0530

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -282,8 +282,8 @@ class MnliProcessor(DataProcessor):
       if i == 0:
         continue
       guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
-      text_a = tokenization.convert_to_unicode(line[8])
-      text_b = tokenization.convert_to_unicode(line[9])
+      text_a = tokenization.convert_to_unicode(line[1])
+      text_b = tokenization.convert_to_unicode(line[2])
       if set_type == "test":
         label = "contradiction"
       else:
