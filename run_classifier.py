730d840e2071568d96809756cb67ebf53e6ac5b4
Soumya Sharma <soumya13106@iiitd.ac.in>
2019-04-01 03:17:30 +0530

    Update run_classifier.py
    Modified file names

--- run_classifier.py
+++ run_classifier.py
@@ -263,13 +263,13 @@ class MnliProcessor(DataProcessor):
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev_matched.tsv")),
-        "dev_matched")
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")),
+        "dev")
 
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test_matched.tsv")), "test")
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
   def get_labels(self):
     """See base class."""
